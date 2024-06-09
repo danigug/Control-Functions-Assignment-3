982b65361f58d0b87ab87ed591ce6b66878f5d56
MatGrav <matteo.gravagnone@gmail.com>
2024-06-09 18:38:24 +0200

    Code cleaning and draft for skidpad and avoidance code, model cleaning

--- script.m
+++ script.m
@@ -1,6 +1,7 @@
 clear all; 
-% close all;
+close all;
 clc
+
 %% Parameters
 m = 1575; % [Kg] vehicle_mass
 Jz = 2875; % [kg*m^2] yaw_mass_moment_inertia
@@ -12,25 +13,11 @@ CR = 2*60000; % [N/rad] rear_axle_cornering_stiffness
 
 delta_max = 25; % [deg] maximum_steering_angle
 
-Vv = 80/3.6; % [m/s]
-
-Tsim = 1500;
-%% State Space definition
-% x = [beta, psi_dot]
-% u = [delta_f, delta_r]
-% y = [beta, psi_dot, rho_G, alfa_f, alfa_r, a_y]
-
-% G = ss(A,B,C,D);
-V=Vv;
-
-
-
-%sim("model.slx")
-
 %% Look-up table of control gains
 N_SAMPLES = 13;
 MAX_SPEED = 130; % [km/h]
 DELTA_SPEED = MAX_SPEED/N_SAMPLES;
+poles = [-10 -20 -100 -200];
 
 B1_c = [0
     CF/m
@@ -49,27 +36,23 @@ for i = 1:N_SAMPLES
     0 0 0 1
     0 (CR*b-CF*a)/(Jz*V(i)) (CF*a-CR*b)/Jz -(CR*b^2+CF*a^2)/(Jz*V(i))];
 
-    K_lookup(i,:) = place(AM{i},B1_c,[-10 -20 -100 -200]);
+    K_lookup(i,:) = place(AM{i},B1_c, poles);
     Kff_lookup(i) = ((m*V(i)^2)/l)*(b/CF-a/CR+(a*K_lookup(i,3)/CR))+l-b*K_lookup(i,3);
 
 end
 
-%% Test
-
-V = 55/3.6;
-
-% sim("model.slx");
-
 %% Evaluation along the relevant curvature profile
-selector = 2;
-% close all
+close all;
+
+curvature_profile = 1;
+speed_profile = 1;
 
 % TO DO: evaluate for different tunings
 
 save_files = false;
 
-V = 60/3.6;
-Tsim = 60;
+V = 80/3.6;
+Tsim = 200;
 
 sim("model.slx");
 
@@ -81,8 +64,8 @@ xlabel('[s]'); ylabel('[m^-1]');
 plot(tout,Kl_profile, 'LineWidth', 1)
 axis tight;  
 currentYlim = ylim;  
-padding = 0.1 * (currentYlim(2) - currentYlim(1));  
-ylim([currentYlim(1) - padding, currentYlim(2) + padding]);  % Set new y-axis limits
+padding = 0.07 * (currentYlim(2) - currentYlim(1));  
+ylim([currentYlim(1) - padding, currentYlim(2) + padding]);
 
 output_dir = "Results";
 
@@ -122,11 +105,88 @@ end
 
 %% Skid-pad test
 
+close all;
+
+curvature_profile = 2;
+speed_profile = 4;
+
 % TO DO
+
+Tsim = 100;
+
+save_files = true;
+sim("model.slx");
+
+name_fig = sprintf('Skidpad test: Lateral deviation');
+fig = figure('Name',name_fig);
+hold on, grid on
+set(gca,'FontName','Times New Roman','FontSize',12)
+xlabel('[s]'); ylabel('[m]');
+plot(tout,e1, 'LineWidth', 1)
+axis normal
+
+if save_files == true
+    filename = sprintf('%s\\Skidpad_lateral_deviation.png',output_dir);
+    saveas(fig, filename);
+end
+
+name_fig = sprintf('Skidpad: Reference and actual trajectory');
+fig = figure('Name',name_fig);
+hold on, grid on
+set(gca,'FontName','Times New Roman','FontSize',12)
+xlabel('[m]'); ylabel('[m]');
+plot(Var_trajectory(:,1),Var_trajectory(:,2),'g',X,Y,'b--','LineWidth', 1.5);
+axis equal
+legend('Real','Ideal','Location', 'best')
+
+
+if save_files == true
+    filename = sprintf('%s\\Skidpad_trajectory.png',output_dir);
+    saveas(fig, filename);
+end
 
 %% Obstacle avoidance manoeuvre
 
+close all;
+
+curvature_profile = 4;
+speed_profile = 1;
+V = 85/3.6;
+
 % TO DO
+
+Tsim = 30;
+
+save_files = true;
+sim("model.slx");
+
+name_fig = sprintf('Avoidance test: Lateral deviation');
+fig = figure('Name',name_fig);
+hold on, grid on
+set(gca,'FontName','Times New Roman','FontSize',12)
+xlabel('[s]'); ylabel('[m]');
+plot(tout,e1, 'LineWidth', 1)
+axis normal
+
+if save_files == true
+    filename = sprintf('%s\\Avoidance_test_deviation.png',output_dir);
+    saveas(fig, filename);
+end
+
+name_fig = sprintf('Avoidance test: Reference and actual trajectory');
+fig = figure('Name',name_fig);
+hold on, grid on
+set(gca,'FontName','Times New Roman','FontSize',12)
+xlabel('[m]'); ylabel('[m]');
+plot(Var_trajectory(:,1),Var_trajectory(:,2),'g',X,Y,'b--','LineWidth', 1.5);
+axis normal
+legend('Real','Ideal','Location', 'best')
+
+
+if save_files == true
+    filename = sprintf('%s\\Avoidance_test_trajectory.png',output_dir);
+    saveas(fig, filename);
+end
 
 %%
 
